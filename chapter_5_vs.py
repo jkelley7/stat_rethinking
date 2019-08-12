81d62aef72a283af7c92cad92b5de78e1af60ade
jkelley7 <jkelleypga@gmail.com>
2019-08-11 20:40:53 -0500

    initial chapter 5 in vs code

---
+++ chapter_5_vs.py
@@ -0,0 +1,16 @@
+#%%
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+import arviz as az
+from collections import Counter
+from scipy.interpolate import griddata
+import pymc3 as pm
+import theano as tt
+
+#%% [markdown]
+# # Chapter 5
+
+#%%
