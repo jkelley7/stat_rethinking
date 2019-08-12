465ba20f1ba7f6a1cbd9ac2f4199ef3073975512
jkelley7 <jkelleypga@gmail.com>
2019-08-11 20:58:41 -0500

    thru 5.1

--- chapter_5_vs.py
+++ chapter_5_vs.py
@@ -14,3 +14,38 @@ import theano as tt
 # # Chapter 5
 
 #%%
+d = pd.read_csv('.\data\WaffleDivorce.csv', sep = ';')
+d.columns = d.columns.str.lower()
+d.head()
+
+#%%
+d['medianagemarriage_s'] = (d.medianagemarriage - d.medianagemarriage.mean())/ d.medianagemarriage.std()
+
+#%% [markdown]
+# ## Code 5.1
+#%%
+shared_x = tt.shared(d.medianagemarriage_s.values)
+shared_y = tt.shared(d.divorce.values)
+with pm.Model() as m51:
+    alpha = pm.Normal('alpha', mu = 10, sigma = 10)
+    beta = pm.Normal('beta', mu = 0, sigma = 1)
+    mu = pm.Deterministic('mu', alpha + beta*shared_x)
+    sigma = pm.Uniform('sigma', lower = 0, upper = 10)
+    divorce = pm.Normal('divorce',mu = mu, sigma = sigma, observed = shared_y)
+    trace51 = pm.sample(draws = 1000,tune = 1000)
+
+#%%
+varnames = ['alpha', 'beta','sigma']
+pm.summary(trace51, varnames = varnames)
+
+#%%
+new_x_values = np.linspace(-3,3.5,num = 30)
+shared_x.set_value(new_x_values)
+shared_y.set_value(np.repeat(0, repeats = len(new_x_values)))
+
+#%%
+with m51:
+    post_pred = pm.sample_posterior_predictive(trace51,samples = 400,model=m51)
+
+
+#%%
