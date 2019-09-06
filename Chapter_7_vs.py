ecb7ff6e745ccc0f7857fb4979df45393669eeff
jkelley7 <jkelleypga@gmail.com>
2019-09-06 11:29:00 -0500

    begin chapter 7

---
+++ Chapter_7_vs.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+import arviz as az
+from collections import Counter
+from scipy.interpolate import griddata
+import pymc3 as pm
+import theano as tt
+import statsmodels.api as sm
+import scipy as sc
+
+sns.set()
